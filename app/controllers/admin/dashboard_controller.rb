3b3e9a859b56345830ebb5e0e0afc83ed39afde3
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-04-15 23:41:43 +0000

    add product count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,14 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "jungle", password: "book"
 
   def show
+
+    @quantity_all = 0
+    Product.find_each do |product|
+      @quantity_all += product.quantity
+    end
+    @quantity_all
   end
+
+  @category_all = Category.count
+
 end
