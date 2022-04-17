a13f9a072ed7a12752dbe1c8bfbf9c9637c221cb
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-04-17 04:40:23 +0000

    add basic autheticator for admin category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "jungle", password: "book"
 
   def index
     @categories = Category.order(id: :desc).all
