ac9645cb8088e779164c9bd010fa4789f88407bc
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-04-18 04:20:44 +0000

    add conditional logic in nav to implicitly show logged in status

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
