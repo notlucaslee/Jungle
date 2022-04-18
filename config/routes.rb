2a218946e3127a3ad5f48eaa92d807323ea29b61
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-04-18 03:58:44 +0000

    add user signup route, model, migration

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
